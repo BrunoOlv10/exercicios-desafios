f42dba91d151651e4ac74db005331a6cfdb58dbd
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-06-19 23:21:35 -0300

    Update function.js

---
+++ exs-rocketseat-js/ex07 - functions/function.js
@@ -0,0 +1,12 @@
+// criar app frases motivacionais
+
+// declaration - declaração da função
+// function statement 
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência é persistência')
+    console.log('Revisão é mão do aprendizado')
+}
+
+// executar, chamar a função
+createPhrases()
