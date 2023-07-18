8f70c6b40171a76f070bb705e90f5ca7e8cea10c
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-07-18 20:05:41 -0300

    test

--- exs-rocketseat-js/ex23 - selecionando elementos/querySelectorAll/script.js
+++ exs-rocketseat-js/ex23 - selecionando elementos/querySelectorAll/script.js
@@ -1,5 +1,6 @@
 // querySelectorAll() - exs: meta (nome elemento), .one (classe igual css ou com # se fosse id), [src] (source que liga o html com outros arquivos)
 
 const elements = document.querySelectorAll('.one');
+// console.log(elements)
 
 elements.forEach(el => console.log(el));
