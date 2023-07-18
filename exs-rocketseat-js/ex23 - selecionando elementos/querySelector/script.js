5d711f0e2ee8f287e9528f775555f1f8bdff30d3
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-07-18 19:47:32 -0300

    add query selector

---
+++ exs-rocketseat-js/ex23 - selecionando elementos/querySelector/script.js
@@ -0,0 +1,4 @@
+// querySelector() - pega apenas o primeiro que ele achar - exs: meta (nome elemento), .one (classe igual css), [src] (source que liga o html com outros arquivos)
+
+const element = document.querySelector('[src]');
+console.log(element);
