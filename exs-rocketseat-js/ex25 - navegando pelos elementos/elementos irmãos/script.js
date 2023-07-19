0e2151d15845b4c891153ff02daa74722064ec57
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-07-19 19:11:35 -0300

    add elementos irmãos

---
+++ exs-rocketseat-js/ex25 - navegando pelos elementos/elementos irmãos/script.js
@@ -0,0 +1,13 @@
+// Navegando pelos elementos 
+// nextSibling e previousSibling - considera os espaços vazios
+// nextElementSibling e previousElementSibling - não considera os espaços
+
+const el = document.querySelector('body script')
+// nextSibling nextElementSibling
+// console.log(el.nextSibling)
+// console.log(el.nextElementSibling)
+
+
+// previousSibling previousElementSibling
+// console.log(el.previousSibling)
+console.log(el.previousElementSibling)
