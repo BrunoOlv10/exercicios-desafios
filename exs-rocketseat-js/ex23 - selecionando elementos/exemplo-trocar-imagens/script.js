3ef51ddb57f11147e5836440acafb91ae1076623
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-07-27 23:13:53 -0300

    add exemplo

---
+++ exs-rocketseat-js/ex23 - selecionando elementos/exemplo-trocar-imagens/script.js
@@ -0,0 +1,10 @@
+function mudarImagem() {
+    var image = document.getElementById('fig1').getElementsByTagName('img')[0];
+    image.src = 'face-logo-azul.png';
+  }
+  
+  function voltarImagem() {
+    var image = document.getElementById('fig1').getElementsByTagName('img')[0];
+    image.src = 'face-logo.png';
+  }
+  
